2bbec62f83249d47464e0abf56c82251db8f3d58
alex <arodriguez202020@gmail.com>
2021-06-16 13:02:45 -0500

    adds products Lego and Air Jordan

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.create(name: "Lego", price: 5, image_url: "https://en.wikipedia.org/wiki/Lego#/media/File:LEGO_logo.svg", description: "A line of plastic construction toys that are manufactured by The Lego Group.")
+
+product = Product.create(name: "Air Jordan 1", price: 100, image_url: "https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_auto,w_1400/fl_lossy,pg_1/qsdrtn3exos5qn5zkvg8/air-jordan-1-chicago?fimg-ssr-default", description: "The Air Jordan 1 High debuted in 1985 as the first signature sneaker developed by Nike for Michael Jordan.")
