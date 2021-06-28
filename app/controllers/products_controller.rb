1fdbd62e4d723d220e7da8c24f1755dc47afd5d8
alex <arodriguez202020@gmail.com>
2021-06-28 14:53:41 -0500

    add user authentication to index action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,12 @@
 class ProductsController < ApplicationController
 
-  def all_products
-    render json: Product.all
-  end
-
-  def lego
-    render json: Product.find_by(name: "Lego")
-  end
-
-  def air_jordan
-    render json: Product.find_by(name: "Air Jordan 1")
-  end
-
-  def one_product
-    product_id = params["id"]
-    product = Product.find_by(id: product_id)
-    render json: product
-  end
 
   def index
+    if current_user
     products = Product.all
     render json: products
+    else
+      render json: []
   end
 
   def show
