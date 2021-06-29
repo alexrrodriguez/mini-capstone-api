c8330531c7816226938739f16073300bf88671b5
alex <arodriguez202020@gmail.com>
2021-06-29 16:52:53 -0500

    delete old product routes from routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  
-  get "/all_products", controller: "products", action: "all_products"
-
-  get "/lego", controller: "products", action: "lego"
-
-  get "/air_jordan", controller: "products", action: "air_jordan"
-
-  get "/one_product" => "products#one_product"
-
-  get "/one_product/:id" => "products#one_product"
 
   get "/products" => "products#index"
 
