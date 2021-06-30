0cbe56ea663a7e053c39d0fa3e7aa3a7e4e609f1
alex <arodriguez202020@gmail.com>
2021-06-30 11:52:56 -0500

    add current user login authentication to index and create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,37 @@
 class OrdersController < ApplicationController
   
   def index
-    orders = current_user.orders
-    render json: orders
+    if current_user
+      orders = current_user.orders
+      render json: orders
+    else
+      render json: [], status: :unauthorized
+    end
   end
 
   def create
-    product = Product.find_by(id: params[:product_id])
-    calculated_subtotal = product.price * params[:quantity].to_i
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
+    if current_user
+      product = Product.find_by(id: params[:product_id])
+      calculated_subtotal = product.price * params[:quantity].to_i
+      calculated_tax = calculated_subtotal * 0.09
+      calculated_total = calculated_subtotal + calculated_tax
 
-    order = Order.new(
-      user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total
-    )
-      if order.save
-        render json: order
-      else
-        render json: {errors: order.errors.full_messages}
-      end
+      order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: calculated_subtotal,
+        tax: calculated_tax,
+        total: calculated_total
+      )
+        if order.save
+          render json: order
+        else
+          render json: {errors: order.errors.full_messages}
+        end
+    else
+      render json: [], status: :unauthorized
+    end
   end
 
   def show
