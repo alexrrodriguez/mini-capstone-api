f2389c15f5bd0df81d628af55e53ebb8fb2e5a33
alex <arodriguez202020@gmail.com>
2021-07-07 13:56:26 -0500

    update order create method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,15 +7,21 @@ class OrdersController < ApplicationController
   end
 
   def create
-    users_carted_products = current_user.cartedProducts.where("status = ?", "carted")
-    subtotal = 0
-    users_carted_products.each do |carted_product|
-      subtotal = carted_product.product.price * 
+    carted_products = current_user.cartedProducts.where("status = ?", "carted")
+    calculated_subtotal = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price 
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
     order = Order.new(
-      users_carted_products[0].product.price * users_carted_products[0].quantity
+      user_id: current_user.id,
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
     )
       if order.save
-        render json: order
+        carted_products.update_all(status: "purchased", order_id: order.id)
+        render json: order.as_json
       else
         render json: {errors: order.errors.full_messages}
       end
